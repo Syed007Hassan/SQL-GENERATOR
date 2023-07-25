fd415c683a2c9c278342a94a12ade0a136e038e1
hassan-ali-OTS <hassan.ali@onetechnologyservices.com>
2023-07-25 11:23:32 +0500

    bard with langhain

--- backend/src/controllers/sql.controller.ts
+++ backend/src/controllers/sql.controller.ts
@@ -1,28 +1,16 @@
-import { Configuration, OpenAIApi } from "openai";
 import { Request, Response } from "express";
-
-const apiKey: string = process.env.OPEN_API_KEY;
-
-const configuration = new Configuration({
-  apiKey: apiKey,
-});
-
-const openapi = new OpenAIApi(configuration);
 
 const getCompletions = async (req: Request, res: Response) => {
   try {
     console.log(req.body.message);
-    const completion = await openapi.createChatCompletion({
-      model: "gpt-3.5-turbo",
-      messages: [
-        {
-          role: "user",
-          content: "Give a SQL statement query for: " + req.body.message + "\n",
-        },
-      ],
-    });
+    const Bard = await import("bard-ai");
+    await Bard.init(
+      "ZAimvVpzJ326bddqrv_DBDNMGdtJQUvPEoVNJZ2GtDe2FXgRxQwcYMf7qKzq3K6llmsmbA."
+    );
+    const completion = await Bard.askAI(req.body.message);
+    console.log(completion);
 
-    res.status(200).json({ message: completion.data.choices[0].message });
+    res.status(200).json({ message: completion });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
